3b5a75c1da879c187ebac8499874e143e4ad75b2
Thomas Yee <tyee@ucvts.org>
2017-01-25 17:50:28 +0000

    PUSH!

--- letters.py
+++ letters.py
@@ -1,6 +1,6 @@
 import string
 import pandas as pd
-
+from bokeh.charts import Bar, save, output_file
 # open file, create string
 file = open('youth.txt', 'r')
 raw_string = file.read()
@@ -12,10 +12,7 @@ for char in new_string:
 #  add only the characters we want to our cleaned string
     if char in string.ascii_letters:
 #         print(char)
-        cleaned_string += char
-    elif char in string.digits:
-#         print(char)
-        cleaned_string += char
+        cleaned_string += char + ' '
     elif char in string.whitespace:
 #         print(' ')
         cleaned_string += ' '
@@ -40,4 +37,8 @@ for key, value in word_count.items():
 
 df = pd.DataFrame({'word': words, 'count': count})
 
-print(df)
+chart = Bar(df,"word", title = "Frequency of Letters", values="count",plot_width=1280,plot_height=720,bar_width=.4)
+
+output_file = 'barchart.html'
+
+save(chart)
